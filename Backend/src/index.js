cc77862f2e080ed63ce3573ad4e4bcdc2631f5b2
Akshay Pise <akshaypise1112@gmail.com>
2024-06-01 15:37:12 +0530

    Writing Utils Function

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,15 @@
 import dotenv from 'dotenv';
 import connected_DB from './database/index.js';
+import { app } from './app.js';
 
 dotenv.config({ path: './env' });
 
-connected_DB();
+connected_DB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port : ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log('MongoDB connection failed...', err);
+    });
