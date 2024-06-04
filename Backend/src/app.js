36fa71aef340a25592a70db62305fde65969b2a2
Akshay Pise <akshaypise1112@gmail.com>
2024-06-04 10:12:03 +0530

    connecting frontend to backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,15 +3,15 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
-
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: '16kb' }));
-
 app.use(express.urlencoded({ extended: true, limit: '16kb' }));
-
 app.use(express.static('static'));
-
 app.use(cookieParser());
+
+//routes
+import router from './routes/index.routes.js';
+
+app.use('/api', router);
 
 export { app };
